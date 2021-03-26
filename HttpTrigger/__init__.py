28de40c0369391dae1af6bf1c749cbd377f15ef5
Stefanus Hinardi <shinardi@microsoft.com>
2021-03-25 21:27:20 -0700

    Adding pyodbc for testing

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 import azure.functions as func
-
+import pyodbc
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
